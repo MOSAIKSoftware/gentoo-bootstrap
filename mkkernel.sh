107f2850cb2e669253ace9c5e58dc63d6a06aaf2
Christian Holzberger <ch@mosaik-software.de>
2016-01-16 18:22:34 +0100

    No modules n kernel config

--- mkkernel.sh
+++ mkkernel.sh
@@ -13,6 +13,6 @@ cp /etc/paludis/kernel/config "${kernel_dir}"/.config || die
 cd "${kernel_dir}" || die
 make olddefconfig || die
 make -j$(nproc 2>/dev/null || echo '1') || die
-make modules_install || die
+#make modules_install || die # no modules support in kernel
 cp arch/x86/boot/bzImage \
 	/boot/kernel-$(readlink "${kernel_dir}" | sed "s/linux-//") || die
