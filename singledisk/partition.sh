2c31b976333f36a30604ee6dd4d01910c7286338
Christian Holzberger <ch@mosaik-software.de>
2016-02-03 19:33:00 +0100

    partition using here doc

--- singledisk/partition.sh
+++ singledisk/partition.sh
@@ -1,21 +1,32 @@
-# Raid Partition Profile 
+# Singledisk Partition Profile 
 
 # partitioning
 function bs_partition_disk_profile_create () {
-  parted -s ${disk} mklabel gpt
-	parted -s ${disk} mkpart primary 1MiB 4MiB || die "failed creating BIOS boot partition"
-	parted -s ${disk} set 1 bios_grub on || die "failed setting bios_grub flag BIOS boot partition"
-	parted -s ${disk} mkpart primary 4MiB 300MiB || die "failed creating /boot partition"
-	parted -s ${disk} mkpart primary 300MiB 2300MiB || die "failed creating swap partition"
-	parted -s ${disk} mkpart primary 2300MiB 66% || die "failed creating root partition"
-	parted -s ${disk} mkpart primary 66% 100% || die "failed creating btrfs partition"
+	parted -a optimal -s "${disk}" <<-EOS
+		mklabel gpt
+		mkpart primary 1MiB 4MiB
+		set 1 bios_grub on 
+		set 1 name "grub"
+		mkpart primary 4MiB 300MiB 
+		set 2 name "boot"
+		set 2 legacy_boot on
+		mkpart primary 300MiB 2300MiB 
+		set 3 name "swap"
+		mkpart primary 2300MiB 66% 
+		set 4 name "root"
+		mkpart primary 66% 100% 
+		set 5 name "docker"
+EOS
+
+[[ "$?" != "0" ]] || die "failed setting up partitions" 
+
 	partprobe ${disk}
 }
 
 function bs_partition_disk_profile_mkfs () {
-	mkfs.ext4 -L root ${disk}4 || die "failed creating ext4 on ${disk}4"
-	mkswap -L swap ${disk}3 || die "failed creating swap partition on ${disk}1"
 	mkfs.ext2 -L boot ${disk}2 || die "failed creating ext2 on ${disk}2"
+	mkswap -L swap ${disk}3 || die "failed creating swap partition on ${disk}3"
+	mkfs.ext4 -L root ${disk}4 || die "failed creating ext4 on ${disk}4"
 	mkfs.btrfs -L docker ${disk}5 || die "failed creating btrfs filesystem on ${disk}5"
 }
 
