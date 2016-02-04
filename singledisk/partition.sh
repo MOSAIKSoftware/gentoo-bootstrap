22a133cdb7a6ec05a8d4b899e553e7f7b86b6aba
Christian Holzberger <ch@mosaik-software.de>
2016-02-04 03:24:47 +0100

    fix partitioning

--- singledisk/partition.sh
+++ singledisk/partition.sh
@@ -2,29 +2,31 @@
 
 # partitioning
 function bs_partition_disk_profile_create () {
-cat <<-EOF |	parted -a optimal -s "${disk}" 
-		mklabel gpt
-		mkpart primary 1MiB 300MiB
-		set 1 name "boot"
-		set 1 legacy_boot on
-		mkpart primary 300MiB 2300MiB 
-		set 2 name "swap"
-		mkpart primary 2300MiB 66% 
-		set 3 name "root"
-		mkpart primary 66% 100% 
-		set 4 name "docker"
-EOF
+part=(
+		'mklabel gpt'
+		'mkpart primary 1MiB 300MiB'
+		'name 1 "boot"'
+		'set 1 legacy_boot on'
+		'mkpart primary 300MiB 2300MiB' 
+		'name 2 "swap"'
+		'mkpart primary 2300MiB 66%' 
+		'name 3 "root"'
+		'mkpart primary 66% 100%' 
+		'name 4 "docker"'
+		)
 
-[[ "$?" != "0" ]] && die "failed setting up partitions" 
+		for cmd in "${part[@]}"; do
+			parted -a optimal -s "${disk}" || die "failed setting up partitions: $cmd"
+		done	
 
 	partprobe ${disk}
 }
 
 function bs_partition_disk_profile_mkfs () {
-	mkfs.ext2 -L boot ${disk}1 || die "failed creating ext2 on ${disk}2"
-	mkswap -L swap ${disk}2 || die "failed creating swap partition on ${disk}3"
-	mkfs.ext4 -L root ${disk}3 || die "failed creating ext4 on ${disk}4"
-	mkfs.btrfs -L docker ${disk}4 || die "failed creating btrfs filesystem on ${disk}5"
+	mkfs.ext2 -L boot ${disk}1 || die "failed creating ext2 on ${disk}1"
+	mkswap -L swap ${disk}2 || die "failed creating swap partition on ${disk}2"
+	mkfs.ext4 -L root ${disk}3 || die "failed creating ext4 on ${disk}3"
+	mkfs.btrfs -L docker ${disk}4 || die "failed creating btrfs filesystem on ${disk}4"
 }
 
 function bs_partition_disk_profile_mount () {
